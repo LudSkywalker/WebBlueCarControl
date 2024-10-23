4ee51f33d46ffcc52108a9ba37cfe858218739ac
LudSkywalker <ludingnumpaque@gmail.com>
2024-10-22 22:35:34 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import BluetoothTerminal from "bluetooth-terminal";
+import "./App.css";
+function App() {
+	const [direction, setDirection] = useState("");
+	const [connected, setConnected] = useState(false);
+
+	let terminal = new BluetoothTerminal();
+	terminal.receive = (data: any) => {
+		alert(data);
+	};
+
+	let toggleConnect = () => {
+		if (!connected) {
+			terminal.connect().then(() => {
+				alert(terminal.getDeviceName() + " is connected!");
+			});
+		} else {
+			terminal.disconnect();
+		}
+		setConnected(!connected);
+	};
+	let changeDirection = (direction: string) => {
+		if (connected) {
+			terminal.send(direction);
+			setDirection(direction);
+		}
+	};
+	return (
+		<>
+			<div className="card">
+				<button onClick={toggleConnect}>
+					{!connected ? "Connect" : "Disconnect"}
+				</button>
+			</div>
+			<div className="card">
+				<button onClick={() => changeDirection("!Up")}>Up</button>
+				<button onClick={() => changeDirection("!Down")}>Down</button>
+				<button onClick={() => changeDirection("!Left")}>Left</button>
+				<button onClick={() => changeDirection("!Right")}>Right</button>
+				<p>direction : {direction}</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
